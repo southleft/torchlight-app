9b1065f0c80e6cb3e20868de4c750467908d1a83
csnizik <csnizik@tulane.edu>
2021-08-05 15:36:15 -0500

    hide footer in webview

--- ChildLogin.js
+++ ChildLogin.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { View } from 'react-native';
 import AnimatedLoader from './AnimatedLoader';
 
 export default function ChildLogin() {
+  const removeFooter = `
+  document.querySelector("div.layoutFooterContainer").setAttribute("hidden", "true");
+  true;
+  `;
   return (
-    <WebView
-      source={{ uri: 'https://child.torchlight.care/' }}
-      startInLoadingState={true}
-      renderLoading={() => <AnimatedLoader />}
-    />
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: 'https://child.torchlight.care/' }}
+        startInLoadingState={true}
+        renderLoading={() => <AnimatedLoader />}
+        injectedJavaScript={removeFooter}
+        onMessage={(event) => {
+          console.log('event: ', event)
+        }}
+      />
+    </View>
   );
 }
--- ElderLogin.js
+++ ElderLogin.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { View } from 'react-native';
 import AnimatedLoader from './AnimatedLoader';
 
 export default function ElderLogin() {
+  const removeFooter = `
+  document.querySelector("div.layoutFooterContainer").setAttribute("hidden", "true");
+  true;
+  `;
   return (
-    <WebView
-      source={{ uri: 'https://elder.torchlight.care/' }}
-      startInLoadingState={true}
-      renderLoading={() => <AnimatedLoader />}
-    />
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: 'https://elder.torchlight.care/' }}
+        startInLoadingState={true}
+        renderLoading={() => <AnimatedLoader />}
+        injectedJavaScript={removeFooter}
+        onMessage={(event) => {
+          console.log('event: ', event)
+        }}
+      />
+    </View>
   );
 }
