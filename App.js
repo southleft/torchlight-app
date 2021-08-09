4df7a322a961d6f9ee451d57d7ca4b933a6f2a59
csnizik <csnizik@tulane.edu>
2021-08-09 16:19:41 -0500

    navbar at bottom

--- App.js
+++ App.js
@@ -23,8 +23,9 @@ export default function App() {
     return <AnimatedLoader />;
   } else {
     return (
-      <SafeAreaView style={styles.safe}>
-        <NavigationContainer style={styles.container} ref={navigationRef}>
+      <NavigationContainer style={styles.container} ref={navigationRef}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.safe}>
           <Stack.Navigator initialRouteName="Torchlight" headerMode="screen">
             <Stack.Screen
               name="Torchlight"
@@ -35,25 +36,19 @@ export default function App() {
               name="ChildLogin"
               component={ChildLogin}
               options={{
-                headerShown: 'none',
-                headerTitle: 'Back',
-                headerTintColor: '#FFF',
-                headerStyle: { backgroundColor: '#017092' },
+                headerShown: false
               }}
             />
             <Stack.Screen
               name="ElderLogin"
               component={ElderLogin}
               options={{
-                headerShown: 'none',
-                headerTitle: 'Back',
-                headerTintColor: '#FFF',
-                headerStyle: { backgroundColor: '#447945' },
+                headerShown: false
               }}
             />
           </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+        </SafeAreaView>
+      </NavigationContainer>
     );
   }
 }
@@ -64,5 +59,5 @@ const styles = StyleSheet.create({
   },
   container: {
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  }
+  },
 });
--- ChildLogin.js
+++ ChildLogin.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { WebView } from 'react-native-webview';
-import { View } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import AnimatedLoader from './AnimatedLoader';
 
 export default function ChildLogin() {
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState('');
+  const webviewRef = useRef(null);
+
+  let backButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goBack()
+  };
+
+  let frontButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goForward()
+  }
+
   const removeFooter = `
   document.querySelector("div.layoutFooterContainer").setAttribute("hidden", "true");
   true;
   `;
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
+      <View style={styles.tabBarContainer}>
+        <TouchableOpacity onPress={backButtonHandler}>
+          <Text style={styles.button}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={frontButtonHandler}>
+          <Text style={styles.button}>Forward</Text>
+        </TouchableOpacity>
+      </View>
       <WebView
         source={{ uri: 'https://child.torchlight.care/' }}
         startInLoadingState={true}
@@ -18,7 +39,32 @@ export default function ChildLogin() {
         onMessage={(event) => {
           console.log('event: ', event)
         }}
+        ref={webviewRef}
+        onNavigationStateChange={navState => {
+          setCanGoBack(canGoBack)
+          setCanGoForward(canGoForward)
+          setCurrentUrl(navState.url)
+        }}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  tabBarContainer: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 24,
+    paddingRight: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#017092'
+  },
+  button: {
+    color: 'white',
+    fontSize: 18
+  }
+})
